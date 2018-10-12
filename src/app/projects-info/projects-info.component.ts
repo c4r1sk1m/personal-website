a416ec678acbd2cdc1690ccc5ec2d73a25f6a4c4
Chris Kim - RTCJK <c4r1sk1m@gmail.com>
2018-10-12 11:09:00 -0400

    Making changes to project showcase

--- src/app/projects-info/projects-info.component.ts
+++ src/app/projects-info/projects-info.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects-info.component.css']
 })
 export class ProjectsInfoComponent implements OnInit {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  // images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
   constructor() { }
 
